7d28ba50a8165f4cb706a1dba771887ed2dff3cb
uday kiran <60300889+uday2599@users.noreply.github.com>
2021-06-25 19:20:51 +0530

    Update Covid Query for Dashboard.sql

--- Covid Query for Dashboard.sql
+++ Covid Query for Dashboard.sql
@@ -1,4 +1,4 @@
-Tableau Dashboard lisnk:- https://public.tableau.com/app/profile/uday6136/viz/CovidDashboard_16246267378260/Dashboard1
+Tableau Dashboard link:- https://public.tableau.com/app/profile/uday6136/viz/CovidDashboard_16246267378260/Dashboard1
 
 --Sheet1-Table
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
